8c83852bbc903ff7a7634c9ee1e889f040f79792
GiaHuy1106 <giahuy110600@gmail.com>
2026-07-27 15:55:05 +0700

    + Adding new Scripts
    - tile Scrtipts
    - scriptable Object

---
+++ 2048 Infinity/Assets/Assets/Scripts/Tile Scripts/TileColorData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileColorData", menuName = "Scriptable Objects/TileColorData")]
+public class TileColorData : ScriptableObject
+{
+    public Color BackGroundColor;
+    public Color TextColor;
+}
--- 2048 Infinity/Assets/Scenes/Assets/Scripts/TileState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TileState", menuName = "Scriptable Objects/TileState")]
-public class TileState : ScriptableObject
-{
-    public Color BackgroundColor;
-    public Color TextColor; 
-}
